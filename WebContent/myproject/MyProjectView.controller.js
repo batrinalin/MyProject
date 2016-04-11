899a358a9010c604e4bafec6fcc2b78be01724d7
batrinalin <batrin_alin@yahoo.com>
2016-04-11 10:58:17 +0300

    Commit

--- WebContent/myproject/MyProjectView.controller.js
+++ WebContent/myproject/MyProjectView.controller.js
@@ -1,70 +1,41 @@
-sap.ui.define([
-               'sap/m/MessageBox',
-               'sap/ui/core/mvc/Controller', 
-               "sap/ui/model/json/JSONModel"],
-               
-function(MessageBox, Controller, JSONModel){
+sap.ui.define([ 'sap/m/MessageBox', 'sap/ui/core/mvc/Controller',
+		"sap/ui/model/json/JSONModel" ],
+
+function(MessageBox, Controller, JSONModel) {
 	"use strict";
 
-	
-	/*
-	var obj = {
-		value : 42
-	};
-	
-	function f(a, b, c) {
-		return this.value + a + b + c;
-	}
-	
-	var a = f(); //42;
-	var b = f.call(obj, 1, 2 ,3) //42
-	
-	var args = [];
-	
-	
-	var c =  f.apply(obj, args)//42
-	
-	var f2 = f.bind(obj);
-	//diferenta call si apply, la apply pot sa imi constuiesc eu array-ul de parametrii
-	//bind pot sa selectez eu care ii this.ul care vreau sa imi apeleze functia
-	*/
-			
-			
 	return Controller.extend("myproject.MyProjectView", {
 		/**
 		 * Called when a controller is instantiated and its View controls (if
-		 * available) are already created. Can be used to modify the View before it
-		 * is displayed, to bind event handlers and do other one-time
+		 * available) are already created. Can be used to modify the View before
+		 * it is displayed, to bind event handlers and do other one-time
 		 * initialization.
 		 * 
 		 * @memberOf myproject.MyProjectView
 		 */
 		onInit : function() {
-			
+
 			this.addSectionModel = new JSONModel({
-				
-				newEmployee: {
-					name: "",
-					firstName:"",
-					bornDate: "",
-					employeeDate: "",
-					jobName: "",
-					managerId:"",
-					brm:"" 
+
+				newEmployee : {
+					name : "",
+					firstName : "",
+					bornDate : "",
+					employeeDate : "",
+					jobName : "",
+					managerId : "",
+					brm : ""
 				},
-				
-				showAddSectionState : false
-				
+
+				showAddSectionState : false,
+				editButtonState : false,
+				insertButtonState : false,
+
 			});
-			
+
 			this.modelEmployee = new JSONModel({
 
 				pageTitle : "Titlul pagini web",
-
-				buttonPagina : {
-
-					nume : "Apasa pe buton."
-				},
 
 				brmArray : [ {
 					key : "full",
@@ -87,7 +58,7 @@ function(MessageBox, Controller, JSONModel){
 					brm : "Full-Time",
 					jobName : "Portar",
 					pay : "3450",
-					checked:false
+					checked : false
 				}, {
 					id : "007",
 					name : "Mauro",
@@ -98,8 +69,8 @@ function(MessageBox, Controller, JSONModel){
 					brm : "Full-Time",
 					jobName : "Sef vanzari",
 					pay : "7240",
-					checked:false
-				},{
+					checked : false
+				}, {
 					id : "0123",
 					name : "Bulc",
 					firstName : "Sergiu",
@@ -109,24 +80,26 @@ function(MessageBox, Controller, JSONModel){
 					brm : "Full-Time",
 					jobName : "Lucrator Magazie",
 					pay : "2240",
-					checked:true
+					checked : false
 				} ]
 			});
-			
-			this.getView().setModel(this.modelEmployee) //first model added to View
-			this.getView().setModel(this.addSectionModel, "addSection") //second model added to View
+
+			this.getView().setModel(this.modelEmployee) // first model added to
+			// View
+			this.getView().setModel(this.addSectionModel, "addSection") // second
+			// model
+			// added
+			// to
+			// View
 		},
-		
-		uniqueID : function () {
-			 
-			 return Math.random().toString(10).substr(2, 3);
+
+		uniqueID : function() {
+			return Math.random().toString(10).substr(2, 3);
 		},
-		
-		/*
-		 *Functia calculatePay calculeaza salarul in functe de Incdrare
-		 * */
-		calculatePay : function(brm){
-			
+
+		// Functia calculatePay calculeaza salarul in functe de Incdrare
+		calculatePay : function(brm) {
+
 			var pay;
 			var _numberOfWorkDays;
 			var _hoursOfWork;
@@ -136,42 +109,40 @@ function(MessageBox, Controller, JSONModel){
 
 			if (brm == brmArray[0].text) {
 
-				 _numberOfWorkDays = 22;
-				 _hoursOfWork = 8;
-				 _rateOfWork = 25;
+				_numberOfWorkDays = 22;
+				_hoursOfWork = 8;
+				_rateOfWork = 25;
 				pay = _numberOfWorkDays * _hoursOfWork * _rateOfWork;
 
 			} else if (brm == brmArray[1].text) {
-				
-				 _numberOfWorkDays = 22;
-				 _hoursOfWork = 4;
-				 _rateOfWork = 20;
-				
+
+				_numberOfWorkDays = 22;
+				_hoursOfWork = 4;
+				_rateOfWork = 20;
+
 				pay = _numberOfWorkDays * _hoursOfWork * _rateOfWork;
-				
 
 			} else if (brm = brmArray[2].text) {
-				
-				 _numberOfWorkDays = 22;
-				 _hoursOfWork = 8;
-				 _rateOfWork = 35;
-				 _bonus = 20;
-				 
-				 var salar =  (_numberOfWorkDays * _hoursOfWork * _rateOfWork );
-				 
-				 pay = salar + (salar * _bonus)/100;
+
+				_numberOfWorkDays = 22;
+				_hoursOfWork = 8;
+				_rateOfWork = 35;
+				_bonus = 20;
+
+				var salar = (_numberOfWorkDays * _hoursOfWork * _rateOfWork);
+
+				pay = salar + (salar * _bonus) / 100;
 			}
 			return pay;
-			
+
 		},
-		
-		/*
-		 *Functia resetAddSectionLabels reseteaza campurile din panelul de addSection
-		 * */
-		resetAddSectionLabels: function (){
-			
-			var newEmployee = { 
-				name :"",
+
+		// Functia resetAddSectionLabels reseteaza campurile din panelul de
+		// addSection
+		resetAddSectionModel : function() {
+
+			var newEmployee = {
+				name : "",
 				firstName : "",
 				bornDate : "",
 				employeeDate : "",
@@ -179,184 +150,232 @@ function(MessageBox, Controller, JSONModel){
 				managerId : "",
 				brm : ""
 			};
-					
+
 			this.addSectionModel.setProperty("/newEmployee", newEmployee);
 			this.addSectionModel.refresh();
 		},
 
-		showHideAddSection : function (){
-			
-			var state = this.addSectionModel.getProperty("/showAddSectionState");
-			this.addSectionModel.setProperty("/showAddSectionState", !state);
-			
+		cancelButtonAddSection : function() {
+
+			this.showHideAddSection();
+			this.resetAddSectionModel();
+
 		},
-		
-		
-		//Just testing git!
-		
-		confirmDelete : function () {
-			MessageBox.confirm("Confirm the delete?", function(sValue){
+
+		showHideAddSection : function() {
+
+			var state = this.addSectionModel
+					.getProperty("/showAddSectionState");
+			this.addSectionModel.setProperty("/showAddSectionState", !state);
+		},
+
+		deleteCheck : function() {
+
+			var employee = this.modelEmployee.getProperty("/employeeArray");
+			var newEmployeeArray = [];
+
+			for (var i = 0; i < employee.length; i++) {
+				if (!employee[i].checked) {
+					newEmployeeArray.push(employee[i]);
+				}
+			}
+			this.modelEmployee.setProperty("/employeeArray", newEmployeeArray);
+		},
+
+		confirmDelete : function() {
+
+			var addSectionState = this.addSectionModel
+					.getProperty("/showAddSectionState");
+
+			if (addSectionState === true) {
+				//Daca este addSection state pe true il inchid is il pun pe false.
+				this.showHideAddSection();
+				addSectionState = false;
+				this.resetAddSectionModel();
+			}
+
+			MessageBox.confirm("Confirm the delete?", function(sValue) {
 				if (sValue === "OK") {
 					this.deleteCheck();
 				}
 			}.bind(this));
+
+			this.addSectionModel.setProperty("/showAddSectionState",
+					addSectionState);
+			this.addSectionModel.refresh();
 		},
-		
-		checkBoxState : function(e){
-			
+
+		checkBoxState : function(e) {
 			// e este un event bind-uit de proprietatea select
-			//ne da informatii despre obiectul selectat.
-			//console.log(e.getSource().getBindingContext().getObject());
+			// ne da informatii despre obiectul selectat.
+			// console.log(e.getSource().getBindingContext().getObject());
 			this.modelEmployee.refresh(true);
-			
+
 		},
-		editEmployeeSection : function (e) {
-			
+
+		// Aceasta functie, introduce in sectiunea AddSection employee selectat
+		// pt editare
+		editEmployeeSection : function(e) {
+
+			var i = 0;
 			var employee = this.modelEmployee.getProperty("/employeeArray");
-			var incompleteTodos = []  ;
-			var contor = 0;
-			
-			for(var i = 0; i < employee.length; i ++){
-				if(employee[i].checked){
-					incompleteTodos.push(employee[i]);
-					contor++;
+			var addSectionState = this.addSectionModel
+					.getProperty("/showAddSectionState");
+			var noInsertButton = this.addSectionModel
+					.getProperty("/insertButtonState");
+			var editButton = this.addSectionModel
+					.getProperty("/editButtonState");
+
+			noInsertButton = false;
+			editButton = true;
+			for (i = 0; i < employee.length; i++) {
+				if (employee[i].checked === true) {
+
+					if (addSectionState === false) {
+						this.showHideAddSection();
+					}
+
+					var newEmployee = {
+						id : employee[i].id,
+						name : employee[i].name,
+						firstName : employee[i].firstName,
+						bornDate : employee[i].bornDate,
+						employeeDate : employee[i].employeeDate,
+						jobName : employee[i].jobName,
+						managerId : employee[i].managerId,
+						brm : employee[i].brm
+					};
 				}
 			}
-			
-			console.log(contor);
-			
-			if(contor > 1){
-				alert("Please select only one employee");
-			}else if ( contor < 1 ) {
-				alert("Please select a employee");
-			} else if ( contor == 1 ) {
-				
-				var newEmployee = { 
-					id:incompleteTodos[0].id,
-					name :incompleteTodos[0].name,
-					firstName : incompleteTodos[0].firstName,
-					bornDate : incompleteTodos[0].bornDate,
-					employeeDate : incompleteTodos[0].employeeDate,
-					jobName : incompleteTodos[0].jobName,
-					managerId : incompleteTodos[0].managerId,
-					brm : incompleteTodos[0].brm
-				};
-						
-				this.addSectionModel.setProperty("/newEmployee", newEmployee);
-				this.addSectionModel.refresh();
-			}
-			
+
+			this.addSectionModel.setProperty("/newEmployee", newEmployee);
+			this.addSectionModel.setProperty("/insertButtonState",
+					noInsertButton);
+			this.addSectionModel.setProperty("/editButtonState", editButton);
+			this.addSectionModel.refresh();
+
 		},
-		//press="deleteCheck"
-		
-		deleteConfimation:function(){
-			//sap.m.MessageBox.confirm("Approve purchase order 12345?");
-			sap.ui.commons.MessageBox.confirm("You are not authorized to delete this content.\n Please check your access");
-		},
-		
-		deleteCheck : function(){
-			
-			var employee = this.modelEmployee.getProperty("/employeeArray");
-			var newEmployeeArray = [];
-			
-			for(var i = 0; i < employee.length; i ++){
-				if(!employee[i].checked){
-					newEmployeeArray.push(employee[i]);
-				}
-			}
-			
-			this.modelEmployee.setProperty("/employeeArray", newEmployeeArray);
-		},
-		
-		editEmployee : function () {
+
+		editEmployeeAddSectionButton : function() {
 
 			var i = 0;
 			var j = 0;
-			
 			var id = this.addSectionModel.getProperty("/newEmployee/id")
-  			var name = this.addSectionModel.getProperty("/newEmployee/name");
-			var firstName = this.addSectionModel.getProperty("/newEmployee/firstName");
-			var bornDate = this.addSectionModel.getProperty("/newEmployee/bornDate");
-			var employeeDate = this.addSectionModel.getProperty("/newEmployee/employeeDate");
-			var jobName = this.addSectionModel.getProperty("/newEmployee/jobName");
-			var managerId = this.addSectionModel.getProperty("/newEmployee/managerId");
+			var name = this.addSectionModel.getProperty("/newEmployee/name");
+			var firstName = this.addSectionModel
+					.getProperty("/newEmployee/firstName");
+			var bornDate = this.addSectionModel
+					.getProperty("/newEmployee/bornDate");
+			var employeeDate = this.addSectionModel
+					.getProperty("/newEmployee/employeeDate");
+			var jobName = this.addSectionModel
+					.getProperty("/newEmployee/jobName");
+			var managerId = this.addSectionModel
+					.getProperty("/newEmployee/managerId");
 			var brm = this.addSectionModel.getProperty("/newEmployee/brm");
 			var pay = this.calculatePay(brm);
-			
+
 			var employees = this.modelEmployee.getProperty("/employeeArray");
-			
-			for ( i=0; i<employees.length; i++) {
-				if( employees[i].id == id){
-					j=i;
-					employees[i].name=name;
-					employees[i].firstName=firstName;
-					employees[i].bornDate=bornDate;
-					employees[i].employeeDate=employeeDate;
-					employees[i].jobName=jobName;
-					employees[i].managerId=managerId;
-					employees[i].brm=brm;
-					employees[i].pay=pay;
+
+			for (i = 0; i < employees.length; i++) {
+				if (employees[i].id == id) {
+					j = i;
+					employees[i].name = name;
+					employees[i].firstName = firstName;
+					employees[i].bornDate = bornDate;
+					employees[i].employeeDate = employeeDate;
+					employees[i].jobName = jobName;
+					employees[i].managerId = managerId;
+					employees[i].brm = brm;
+					employees[i].pay = pay;
 				}
-				
+
 			}
-			//console.log(employees[i].id);
-			
-			//Read: map, filter, reduce
+			// console.log(employees[i].id);
+
+			// Read: map, filter, reduce
 			this.addSectionModel.setProperty("/newEmployee", {});
 			this.addSectionModel.refresh();
 			this.modelEmployee.refresh();
+			this.showHideAddSection();
 			console.log('angajatul s-a editat.');
 		},
-		
-		
-		//todo: fa check sa se disable daca se check-uie 2
-		checkSelectBox : function (checked)
-		{
+
+		// todo: fa check sa se disable daca se check-uie 2
+
+		/*checkSelectBox : function(checked) {
 			this.modelEmployee.refresh(true);
-			if (checked == true ) 
-				
+			if (checked == true)
+
 				return true;
 			return false;
-		},
-		
-		checkStatus : function (employeeArray) {
-			
-			if(employeeArray == undefined ){
+		},*/
+
+		checkStatus : function(employeeArray) {
+
+			if (employeeArray == undefined) {
 				return false;
 			}
-			
-			var checkCount =0;
-			var i = 0; 
-			
-			for ( i=0; i<employeeArray.length; i++){
-				if ( employeeArray[i].checked ) {
+
+			var checkCount = 0;
+			var i = 0;
+
+			for (i = 0; i < employeeArray.length; i++) {
+				if (employeeArray[i].checked) {
 					checkCount++;
 				}
 			}
 			console.log(checkCount);
-			if (checkCount > 1){
+			if (checkCount > 1) {
 				return false;
 			}
 			return true;
 		},
-		
-		/*
-		 *Functia addNewEmployee adauga in array-ul de employee noul angajat.
-		 * */
-		addNewEmployee : function(event) {
-			
+
+		//este vorba de butonul din afara AddSection
+		addEmployeeButton : function() {
+
+			var addSectionState = this.addSectionModel
+					.getProperty("/showAddSectionState");
+			var noEditButton = this.addSectionModel
+					.getProperty("/editButtonState");
+			var insertButton = this.addSectionModel
+					.getProperty("/insertButtonState");
+
+			this.resetAddSectionModel();
+
+			if (addSectionState === false) {
+				this.showHideAddSection();
+			}
+
+			noEditButton = false;
+			insertButton = true;
+
+			this.addSectionModel.setProperty("/editButtonState", noEditButton);
+			this.addSectionModel
+					.setProperty("/insertButtonState", insertButton);
+			this.addSectionModel.refresh();
+		},
+		// Functia addNewEmployee adauga in array-ul de employee noul angajat.
+		addNewEmployeeAddSectionButton : function(event) {
+
 			var id = this.uniqueID();
-  			var name = this.addSectionModel.getProperty("/newEmployee/name");
-			var firstName = this.addSectionModel.getProperty("/newEmployee/firstName");
-			var bornDate = this.addSectionModel.getProperty("/newEmployee/bornDate");
-			var employeeDate = this.addSectionModel.getProperty("/newEmployee/employeeDate");
-			var jobName = this.addSectionModel.getProperty("/newEmployee/jobName");
-			var managerId = this.addSectionModel.getProperty("/newEmployee/managerId");
+			var name = this.addSectionModel.getProperty("/newEmployee/name");
+			var firstName = this.addSectionModel
+					.getProperty("/newEmployee/firstName");
+			var bornDate = this.addSectionModel
+					.getProperty("/newEmployee/bornDate");
+			var employeeDate = this.addSectionModel
+					.getProperty("/newEmployee/employeeDate");
+			var jobName = this.addSectionModel
+					.getProperty("/newEmployee/jobName");
+			var managerId = this.addSectionModel
+					.getProperty("/newEmployee/managerId");
 			var brm = this.addSectionModel.getProperty("/newEmployee/brm");
 			var pay = this.calculatePay(brm);
-			
+
 			var employees = this.modelEmployee.getProperty("/employeeArray");
-			
+
 			employees.push({
 				id : id,
 				name : name,
@@ -367,12 +386,11 @@ function(MessageBox, Controller, JSONModel){
 				brm : brm,
 				jobName : jobName,
 				pay : pay,
-				checked:true
+				checked : false
 			});
-			
-			
-			var newEmployee = { 
-				name :"",
+
+			var newEmployee = {
+				name : "",
 				firstName : "",
 				bornDate : "",
 				employeeDate : "",
@@ -380,12 +398,12 @@ function(MessageBox, Controller, JSONModel){
 				managerId : "",
 				brm : ""
 			}
-			
+
 			this.addSectionModel.setProperty("/newEmployee", newEmployee);
 			this.modelEmployee.setProperty("/employeeArray", employees);
 			this.modelEmployee.refresh(true);
 			this.addSectionModel.refresh(true);
 		}
-		
+
 	});
 });
